9d8e4d4de4625fc01f92c9c158f91136f9511a46
jopemachine <jopemachine@naver.com>
2021-09-19 02:30:41 +0900

    Upd

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ const input = clipboardy.readSync();
 
 const items = [
   {
-    title: 'Transform Single -> Double',
+    title: 'Single -> Double',
     subtitle: 'Transform all single quotes to double quotes',
     arg: transform(input, '\'', '"'),
   },
   {
-    title: 'Transform Single, Backtick -> Double',
+    title: 'Single, Backtick -> Double',
     subtitle: 'Transform all single quotes, backticks to double quotes',
     arg: transform(transform(input, '\'', '"'), '`', '"')
   },
   {
-    title: 'Transform Double -> Single',
+    title: 'Double -> Single',
     subtitle: 'Transform all double quotes to single quotes',
     arg: transform(input, '"', '\'')
   },
   {
-    title: 'Transform Double, Backtick -> Single',
+    title: 'Double, Backtick -> Single',
     subtitle: 'Transform all double quotes to single quotes',
     arg: transform(transform(input, '"', '\''), '`', '\'')
   },
   {
-    title: 'Transform Single, Double -> Backtick Quotes',
+    title: 'Single, Double -> Backtick Quotes',
     subtitle: 'Transform all quotes to backticks',
     arg: transform(transform(input, '\'', '`'), '\"', '`')
   },
